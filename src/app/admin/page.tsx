b27f84073096fd55143a5f21fb2b503b98d4cddc
rickbody-code <rickbody@gmail.com>
2025-08-30 12:21:46 +1000

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import { prisma } from "@/lib/prisma";
 import { AnnouncementForm } from "@/components/AnnouncementForm";
 import { QuickLinkForm } from "@/components/QuickLinkForm";
